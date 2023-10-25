60633fddbfbd4fd924d21d0619312e7bdeb2ef61
Firman Fachrial <firman.fachrial@gmail.com>
2023-10-25 15:18:58 +0700

    method request

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
  
     response.statusCode = 200;
-    response.end('<h1>Hello HTTP Server!</h1>');
-};
+
+    const { method } = request;
  
+    if(method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+ 
+    if(method === 'POST') {
+        response.end('<h1>Hola!</h1>');
+    }
+ 
+    if(method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+ 
+    if(method === 'DELETE') {
+        response.end('<h1>Moshi-moshi!</h1>');
+    }
+};
  
 const server = http.createServer(requestListener);
  
